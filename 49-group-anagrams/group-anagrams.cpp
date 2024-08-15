a2d27dc611c0f0ccff2e87cde0d9bc0a577da9ca
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-15 12:38:53 +0530

    Time: 53 ms (12.05%) | Memory: 45.3 MB (5.05%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    // std::array<int, 256>hash(string s){
-    //     // make a hash
-    //     std::array<int,256>hash = {0};
-    //     for(int i = 0; i < s.size(); i++){
-    //         hash[s[i]]++;
-    //     }
-    //     return hash;
-    // }
+    std::array<int, 256>hash(string s){
+        // make a hash
+        std::array<int,256>hash = {0};
+        for(int i = 0; i < s.size(); i++){
+            hash[s[i]]++;
+        }
+        return hash;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // create map
-        map<string, vector<string>>mp;
-        // M2 -> map<std::array<int,256>, vector<string>>mp;
+        // map<string, vector<string>>mp;
+        // M2  
+        map<std::array<int,256>, vector<string>>mp;
         
         // loop on strs
         for(auto str:strs){
             // take a string say -> s
-            string s = str;
+            // string s = str;
             // sort s
-            sort(s.begin(), s.end());
-            mp[s].push_back(str);
-            // M2 -> mp[hash(s)].push_back(str);
+            // sort(s.begin(), s.end());
+            // mp[s].push_back(str);
+            // M2  
+            mp[hash(str)].push_back(str);
         }
             
         // make a ans vector<vector<string>>
