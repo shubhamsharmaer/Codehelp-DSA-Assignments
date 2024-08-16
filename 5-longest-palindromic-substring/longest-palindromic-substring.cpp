70a8cf863b59ba6ca1dfc8d101f7aae4679f13b8
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-16 09:50:42 +0530

    Time: 1730 ms (5.01%) | Memory: 491.8 MB (5.49%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string& s, int i, int j){
+        while(i < j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        // take an ans string
+        string ans = "";
+        // loop to get substring
+        for(int i = 0; i < s.size(); i++){
+            for(int j = i; j < s.size(); j++){
+                if(isPalindrome(s,i,j)){
+                    string t = s.substr(i, j - i + 1);
+                    ans = t.size() > ans.size() ? t : ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
