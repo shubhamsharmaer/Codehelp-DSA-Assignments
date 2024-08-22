4709dfaa65b3fecf2480c47206aa59cc219943a3
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-22 10:37:19 +0530

    Time: 7 ms (26.84%) | Memory: 12.8 MB (68.26%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
+        // take 3 var 
+            // 1 index -> for in place changes
+            // 2 count -> for counting chars
+            // 3 prev -> for matching with curr i
+        // loop for travers
+            // if([i] == prev) -> count++;
+            // else 
+                    // [index++] = prev
+                    // if(count > 1) 
+                        // store count in form of chars in reverse -> SEE CODE
+                    // update prev -> prev = i;
+                    // reset count -> count = 1;  
         int index = 0, count = 1;
         char prev = chars[0];
 
@@ -11,12 +23,14 @@ public:
             else{
                 chars[index++] = prev;
                 if(count > 1){
-                    int start = index;
+                    //-----------------------//
+                    int start = index; 
                     while(count){
                         chars[index++] = (count % 10) + '0';
                         count /= 10;
                     }
                     reverse(chars.begin() + start, chars.begin() + index);
+                    //---------------------------------------------------//
                 }
                 prev = chars[i];
                 count = 1;
