b4ffb7d52475d246e20d817eacd78f69eb7ae31c
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-25 21:31:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (74.09%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        size_t pos = haystack.find(needle);
-        if(pos != string :: npos){
-            return pos;
+        // STL Method | QUICK
+        //---------------------------------//
+        // size_t pos = haystack.find(needle);
+        // if(pos != string :: npos){
+            // return pos;
+        // }
+        // return -1;
+        //---------------------------------//
+
+
+        // Method 2 -> 2 Pointer
+        int n = haystack.size();
+        int m = needle.size();
+
+        for(int i = 0; i <= n-m; i++){
+            for(int j = 0; j < m; j++){
+                if(needle[j] != haystack[i+j]){
+                    break;
+                }
+                if(j == m-1){
+                    return i;
+                }
+            }
         }
         return -1;
     }
