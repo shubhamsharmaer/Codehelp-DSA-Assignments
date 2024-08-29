9dee97e5a73d8d532c65a428d5fe62172a1296f1
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-29 12:44:35 +0530

    Time: 5 ms (28.49%) | Memory: 11.1 MB (72.14%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    // Pointers Setup:
+    // Teen pointers use karo: i -> nums1 ke end ke liye, 
+    //                         j -> nums2 ke end ke liye, 
+    //                         k -> ek nums1 ke last position ke liye.
+
+    // Compare from End with Loop: -> while(i >= 0 && j >= 0) 
+    //                                  -> if([i] > [j]) then nums1[k--] = nums[i--]
+    //                                  -> else -> nums1[k--] = nums2[j--]
+
+    // Copy bache hue elem: while(j >= 0) -> nums1[k--] = nums[j--];
+
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m - 1;
         int j = n - 1;
