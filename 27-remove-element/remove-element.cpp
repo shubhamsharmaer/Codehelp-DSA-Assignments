0cf1c76e4e150487a546611c496d430b598aaf70
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-30 11:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (32.35%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // use std::remove to remove val from begin() -> end()
+        // then nums will look like this -> [0,1,3,0,4,?,?,?]
+        // ? -> garbage value
+        // to erase this garbage value from nums in place -> nums.erase(it, nums.end())
+        // return size()
+        auto it = std::remove(nums.begin(), nums.end(), val);
+        nums.erase(it, nums.end());
+        return nums.size();
+    }
+};
