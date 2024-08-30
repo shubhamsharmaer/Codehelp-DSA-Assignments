4ba6dc75a5ce23185acb58b997c7b204a1ac1479
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-30 10:47:31 +0530

    Time: 102 ms (5.27%) | Memory: 70.6 MB (62.07%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // take vars
+// take vars
     //         1 maxLeftBordersum, maxRightBordersum -> INT_MIN
     //         2 recursive call -> maxleftsum, maxrightsum
     //         3 leftbordersum, rightbordersum -> 0
@@ -12,35 +12,34 @@ public:
     //              check if rightbordersum > maxRightBordersum -> maxRightBordersum = rightbordersum
     // take CrossBorderSum = maxRightBordersum + maxLightBordersum
     // return max(CrossBorderSum, max(maxleftsum, maxrightsum))
-
-
     int maxSubArrayHelper(vector<int>& v, int start, int end){
-        // base case
-        if(start == end) return v[start];
         int mid = start + ((end - start) / 2);
+        //base case
+        if(start == end) return v[start];
+
         int maxLeftBordersum = INT_MIN, maxRightBordersum = INT_MIN;
 
-        // recusive call
+        // recursive call
         int maxleftsum = maxSubArrayHelper(v, start, mid);
         int maxrightsum = maxSubArrayHelper(v, mid+1, end);
 
-        // CROSS BORDER SUM
-        int leftbordersum = 0, rightbordersum = 0;
-        // left sum
+        int maxleftbordersum = 0, maxrightbordersum = 0;
+        // loop for leftsum
         for(int i = mid; i >= start; i--){
-            leftbordersum += v[i];
-            if(leftbordersum > maxLeftBordersum) maxLeftBordersum = leftbordersum; 
+            maxleftbordersum += v[i];
+            if(maxleftbordersum > maxLeftBordersum) maxLeftBordersum = maxleftbordersum; 
         }
-        // right sum
+        // loop for rightsum
         for(int i = mid+1; i <= end; i++){
-            rightbordersum += v[i];
-            if(rightbordersum > maxRightBordersum) maxRightBordersum = rightbordersum; 
-        }
+            maxrightbordersum += v[i];
+            if(maxrightbordersum > maxRightBordersum) maxRightBordersum = maxrightbordersum; 
+        } 
+
+        // Cross Border Sum
         int CrossBorderSum = maxRightBordersum + maxLeftBordersum;
-
-        return max(CrossBorderSum, max(maxrightsum, maxleftsum));
-
+        return max(CrossBorderSum, max(maxleftsum, maxrightsum));
     }
+
     int maxSubArray(vector<int>& nums) {
         return maxSubArrayHelper(nums, 0, nums.size()-1);
     }
