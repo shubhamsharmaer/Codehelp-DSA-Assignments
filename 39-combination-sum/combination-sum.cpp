c92529134e5e5241429e09a89518fb845c4b36a0
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-30 12:47:22 +0530

    Time: 4 ms (71.18%) | Memory: 12.5 MB (76.38%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // take an ans vector<vector> to return 
+    // take v vector to store curr element
+    // In combinationSumHelper -> base case: 
+    //                              if target == 0 -> ans.push(v), return 
+    //                              if target < 0 -> return
+    //                          -> loop (i = index ; i < candidates.size())
+    //                              push [i] in v
+    //                              recursive call with target-candidates[i], i
+    //                              v.pop_back()
+    void combinationSumHelper(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& v, int index){
+        // base case
+        if(target == 0){
+            ans.push_back(v);
+            return;
+        }
+        if(target < 0){
+            return;
+        };
+
+        for(int i = index; i < candidates.size(); i++){
+            v.push_back(candidates[i]);
+            combinationSumHelper(candidates, target-candidates[i], ans, v, i);
+            v.pop_back();
+        }
+
+    } 
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        combinationSumHelper(candidates, target, ans, v, 0);
+        return ans;
+    }
+};
