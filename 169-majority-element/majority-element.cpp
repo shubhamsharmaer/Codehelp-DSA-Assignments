08e439e1da33abc9d4ac6cc6bc548e4ef65095d3
Shubham Sharma <shubhamgetmail@gmail.com>
2024-08-31 11:25:35 +0530

    Time: 23 ms (20.95%) | Memory: 28.1 MB (20.30%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,6 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         // sort
         sort(nums.begin(), nums.end());
+        // return mid element
         return nums[nums.size() / 2];
     }
 };
