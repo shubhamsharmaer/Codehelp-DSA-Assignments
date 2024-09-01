edea8b7f3353b458c7abc91f51331824f4650f48
Shubham Sharma <shubhamgetmail@gmail.com>
2024-09-01 11:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (67.09%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -8,18 +8,16 @@ public:
                                             // ans += romansym[i]
                                             // num -= values[i]
         //return ans;
-        string Romansymbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", 
-                                "X", "IX", "V", "IV", "I"};
-        int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-
+       
+       string symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+       int values[] = {1000,900,500,400,100,90,50,40,10, 9, 5, 4, 1};
         string ans = "";
-
-        for(int i = 0; i < 13; i++){
-            while(num >= values[i]){
-                ans += Romansymbols[i];
-                num -= values[i];
-            }
+       for(int i = 0; i < 13; i++){
+        while(num >= values[i]){
+            ans += symbol[i];
+            num -= values[i];
         }
-        return ans;
+       }
+       return ans;
     }
 };
