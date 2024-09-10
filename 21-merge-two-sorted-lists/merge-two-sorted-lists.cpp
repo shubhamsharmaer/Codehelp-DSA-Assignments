59cb3a6892a7d45c88f9c46ae55e683776a346a1
Shubham Sharma <shubhamgetmail@gmail.com>
2024-09-10 11:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (58.59%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // base condn -> if left == 0 return right or right == 0 return left
+    // take Node* ansNode  = new Node(-1);
+    // take Node* mptr = ansNode;
+    // loop while(left && right)
+    //              if(left -> val <= right -> val) then 
+    //              else
+    // add left node if any (left) mptr -> next = left
+    // add right node if any (left) mptr -> next = right
+    ListNode* mergeTwoLists(ListNode* left, ListNode* right) {
+        // base condn
+        if(left == 0) return right;
+        if(right == 0) return left;
+        
+        ListNode* ansNode = new ListNode(-1);
+        ListNode* mptr = ansNode;
+        while(left && right){
+            if(left -> val <= right -> val){
+                mptr -> next = left;
+                mptr = left;
+                left = left -> next;
+            }
+            else{
+                mptr -> next = right;
+                mptr = right;
+                right = right -> next;
+            }
+        }
+
+        if(left){
+            mptr -> next = left;
+            // mptr = left;
+            // left = left -> next;
+        }
+        if(right){
+            mptr -> next = right;
+            // mptr = right;
+            // right = right -> next;
+        }
+        return ansNode -> next;
+    }
+};
