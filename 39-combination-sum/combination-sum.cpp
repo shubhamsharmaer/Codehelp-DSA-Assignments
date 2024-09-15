6b849893f03cdfa7ecaa1e89a0db0b8035dd59aa
Shubham Sharma <shubhamgetmail@gmail.com>
2024-09-16 00:12:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (57.91%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -9,28 +9,27 @@ public:
     //                              push [i] in v
     //                              recursive call with target-candidates[i], i
     //                              v.pop_back()
-    void combinationSumHelper(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& v, int target, int index) {
+    void combinationSumHelper(vector<vector<int>>& ans, vector<int>& v, vector<int>& candidates, int target, int index){
         // base case
         if(target == 0){
             ans.push_back(v);
             return;
         }
-        if(target < 0){
-            return;
-        }
+        if(target < 0) return;
 
-        // loop 
+        // ek case soln
         for(int i = index; i < candidates.size(); i++){
             v.push_back(candidates[i]);
-            combinationSumHelper(candidates, ans, v, target-candidates[i], i);
+            //  RE CALL
+            combinationSumHelper(ans, v, candidates, target - candidates[i], i);
             v.pop_back();
         }
 
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int> v;
-        combinationSumHelper(candidates, ans, v, target, 0);
+        vector<int> v; // to store curr element
+        combinationSumHelper(ans, v, candidates, target, 0);
         return ans;
     }
 };
