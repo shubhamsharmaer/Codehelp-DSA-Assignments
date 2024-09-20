9b9401b8041732cfad9467a980abea25bd58c1d5
Shubham Sharma <shubhamgetmail@gmail.com>
2024-09-21 00:05:35 +0530

    Time: 3 ms (63.55%) | Memory: 14 MB (15.01%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
+    
     vector<int> helper(string exp){
+        // take ans vec
         vector<int> ans;
-        
-        // loop on string
+        // loop on str
         for(int i = 0; i < exp.size(); i++){
-            // cond for opr check
             if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*'){
-                // make two ans
+                // make two ans1 and ans2 with RE Call
                 vector<int> ans1 = helper(exp.substr(0, i));
                 vector<int> ans2 = helper(exp.substr(i+1));
 
-                // loop on both ans1 and ans2
+                // nested loops
                 for(int j = 0; j < ans1.size(); j++){
                     for(int k = 0; k < ans2.size(); k++){
-                        // check for oprts
-                        if(exp[i] == '+') ans.push_back(ans1[j] + ans2[k]);
-                        else if(exp[i] == '-') ans.push_back(ans1[j] - ans2[k]);
-                        else ans.push_back(ans1[j] * ans2[k]);
+                        // check for optrs
+                        if(exp[i] == '+'){
+                            ans.push_back(ans1[j] + ans2[k]);
+                        }
+                        else if(exp[i] == '-'){
+                            ans.push_back(ans1[j] - ans2[k]);
+                        }
+                        else{
+                            ans.push_back(ans1[j] * ans2[k]);
+                        }
                     }
                 }
             }
-            
         }
         // base case
-        if(ans.size() == 0) ans.push_back(stoi(exp));
+        if(ans.size() == 0){
+            ans.push_back(stoi(exp));
+        }
         return ans;
-
     }
     vector<int> diffWaysToCompute(string expression) {
         return helper(expression);
