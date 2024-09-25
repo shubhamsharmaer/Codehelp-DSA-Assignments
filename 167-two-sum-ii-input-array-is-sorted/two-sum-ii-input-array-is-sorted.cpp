6540195ee7cbddd4b431cae198c4a97ab76b6b9c
Shubham Sharma <shubhamgetmail@gmail.com>
2024-09-25 14:08:15 +0530

    Time: 7 ms (82.48%) | Memory: 18.4 MB (12.86%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-     vector<int> ans;
-        int idx1 = 1;  // Start from the first element (0-based index)
-        int idx2 = numbers.size();  // Start from the last element
-
-        // Use two-pointer technique
-        while (idx1 < idx2) {
-        int sum = numbers[idx1 - 1] + numbers[idx2 - 1];
-        if(sum == target){
-            ans.push_back(idx1);
-            ans.push_back(idx2);
-            break;
+        vector<int> ans; // store ans indx
+        int idx1 = 1;
+        int idx2 = numbers.size();
+        while(idx1 < idx2){
+            int sum = numbers[idx1-1] + numbers[idx2-1]; 
+            if(sum == target){
+                ans.push_back(idx1);
+                ans.push_back(idx2);
+                break;
+            }
+            else if(sum < target){
+                idx1++;
+            }
+            else{
+                idx2--;
+            }
         }
-        else if(sum < target){
-            idx1++;
-        }
-        else{
-            idx2--;
-        }
-    }
-    return ans;
+        return ans;
     }
 };
