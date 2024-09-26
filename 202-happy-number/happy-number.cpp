3c3c9368342e5addb3dd5f6c83daa5408fa9d7fd
Shubham Sharma <shubhamgetmail@gmail.com>
2024-09-26 21:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (89.32%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -8,25 +8,26 @@ public:
     
     //  init slow with n and fast funcall(n)
     // while fast != 1 && slow != fast --> call slow one time and fast 2 time
-
     int getnext(int n){
-        int sum = 0; // take a sum int
+        int sum = 0;
+
         while(n > 0){
-            int digit = n % 10; // extract the last digit
-            sum += digit * digit; // take square and add to sum
-            n /= 10; // extract each digit
+            int digit = n % 10;
+            sum += digit*digit;
+            n /= 10;    
         }
-
         return sum;
-
     }
     bool isHappy(int n) {
         int slow = n;
         int fast = getnext(n);
+
         while(fast != 1 && slow != fast){
-            slow = getnext(slow);
             fast = getnext(getnext(fast));
+            slow = getnext(slow);
         }
+
         return fast == 1;
+
     }
 };
