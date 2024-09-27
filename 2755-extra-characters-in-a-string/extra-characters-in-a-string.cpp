d6ff4d1551d6280f974da261b0a73370a5871d9a
Shubham Sharma <shubhamgetmail@gmail.com>
2024-09-27 16:07:50 +0530

    Time: 206 ms (14.48%) | Memory: 96.4 MB (24.50%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
+    // take a set string for dic
+    // take a map int, int for s
+    // call solve (0, s, set, map, n)
+    // logic for solve ---> base i >= n return 0
+    //                 ---> take int ans = i+1, baki same
+    //                 ---> check if [i] count in mp then return mp[i]
+    //                 ---> for loop j = i < n
+    //                      ---> take a curr string = s.substr(i, j-i+1)
+    //                      ---> check if st.count(curr) then ans = min(ans, solve(j+1, same))
+    //          --> return mp[i] = ans;
     int solve(int i, string s, unordered_set<string>& st, int& n, unordered_map<int, int>& mp){
+        // base 
         if(i >= n) return 0;
 
-        int ans = 1 + solve(i+1, s, st, n, mp);
-        
+        // check for string exists in set or not
         if(mp.count(i)){
             return mp[i];
         }
-        // not skip i = 0
-        for(int j  = i; j < n; j++){
-            string cur = s.substr(i, j-i+1);
-            if(st.count(cur)){
-                // valid
+
+        int ans = 1 + solve(i+1, s, st, n, mp);
+        for(int j = i; j < n; j++){
+            string curr = s.substr(i, j-i+1);
+            // check if st has i
+            if(st.count(curr)){
                 ans = min(ans, solve(j+1, s, st, n, mp));
-            }
+            } 
+
         }
         return mp[i] = ans;
-        
     }
-    int minExtraChar(string s, vector<string>& dict) {
+    int minExtraChar(string s, vector<string>& dic) {
         int n = s.size();
-        unordered_set<string> st(begin(dict), end(dict));
         unordered_map<int, int> mp;
-        return solve(0, s, st, n, mp);
-
+        unordered_set<string> st(begin(dic), end(dic));
+        int ans = solve(0, s, st, n, mp);
+        return ans;
     }
 };
