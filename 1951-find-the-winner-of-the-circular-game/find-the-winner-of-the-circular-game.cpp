8ce74a70a70fa8145de351c94bc14e4497e8bde8
Shubham Sharma <shubhamgetmail@gmail.com>
2024-10-22 23:10:25 +0530

    Time: 28 ms (11.95%) | Memory: 26.1 MB (7.89%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    // take a queue
-    // push all from 0 --> n in qu
-    // pop element with k gap
-    // when qu left with one element then it will be winner of the game
     int findTheWinner(int n, int k) {
-        queue<int> qu;
+        // take a queue
+        queue<int> q;
         for(int i = 1; i <= n; i++)
-            qu.push(i);
-
-            while(qu.size() > 1){
-                // simulate the game
-                for(int i = 1; i<k; i++){
-                    // pop the kth
-                    auto player = qu.front();
-                    qu.pop();
-
-                    // take back them as well
-                    qu.push(player);
+            q.push(i);
+        
+            while(q.size() != 1){
+                for(int i = 1; i < k; i++){
+                    auto player = q.front();
+                    q.pop();
+                    // push back the players
+                    q.push(player);
                 }
-                qu.pop();
+                q.pop();
             }
-        return qu.front();
-
+        return q.front();
     }
 };
