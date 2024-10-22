a8e4ff8481b9e700ff90cbcf99f5137eb305ec09
Shubham Sharma <shubhamgetmail@gmail.com>
2024-10-22 23:10:44 +0530

    Time: 29 ms (10.42%) | Memory: 25.8 MB (21.69%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // take a queue
+    // push all from 0 --> n in qu
+    // pop element with k gap ----
+    // when qu left with one element then it will be winner of the game
+
     int findTheWinner(int n, int k) {
         // take a queue
         queue<int> q;
