a4192ea9523b83bcd7aefca5c6d2faebf8dc5615
Shubham Sharma <shubhamgetmail@gmail.com>
2024-10-27 16:12:11 +0530

    Time: 2 ms (24.88%) | Memory: 70.5 MB (70.18%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,7 +5,7 @@ public:
         int cur = nums[0];
         int maxSum = nums[0];
 
-        // loop for calculating the subarray sum
+        // loop to find the max sum
         for(int i = 1; i < nums.size(); i++){
             cur = max(nums[i], cur + nums[i]);
             maxSum = max(maxSum, cur);
