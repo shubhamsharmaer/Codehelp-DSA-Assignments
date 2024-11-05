9fd7957954385f1900d1f88e51999c4301f3a1fb
Shubham Sharma <shubhamgetmail@gmail.com>
2024-11-05 23:52:36 +0530

    Time: 4 ms (41.79%) | Memory: 22.1 MB (22.56%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -5,19 +5,19 @@ public:
     // loop 1 to nums.size() ---> prefix[i] = prefix[i-1] + nums[i]
 
     // return prefix[right] - prefix[left-1]
-    vector<int> prefixsum; // prefix sum
+    vector<int> prefix;
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        prefixsum.resize(n);
-        prefixsum[0] = nums[0];
+        prefix.resize(n);
+        prefix[0] = nums[0];
         for(int i = 1; i < n; i++){
-            prefixsum[i] = prefixsum[i-1] + nums[i];
-        }    
+            prefix[i] += prefix[i-1] + nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        if(left == 0) return prefixsum[right];
-        return prefixsum[right] - prefixsum[left - 1];
+        if(left == 0) return prefix[right];
+        return prefix[right] - prefix[left - 1];
     }
 };
 
