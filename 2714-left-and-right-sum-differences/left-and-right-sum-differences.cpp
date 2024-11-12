6bc53353d667d43d7c6e2ea7592f658fa8d47d61
Shubham Sharma <shubhamgetmail@gmail.com>
2024-11-12 17:17:41 +0530

    Time: 51 ms (5.88%) | Memory: 14.1 MB (22.38%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findprefix(vector<int>& nums){
+        int n = nums.size();
+        vector<int> prefixsum(n, 0);
+        for(int i = 1; i < nums.size(); i++){
+            prefixsum[i] += prefixsum[i - 1] + nums[i - 1]; 
+        }
+        return prefixsum;
+    }
+
+    vector<int> findsuffix(vector<int>& nums){
+        int n = nums.size();
+        vector<int> suffixsum(n, 0);
+        for(int i = n - 2; i >= 0; i--){
+            suffixsum[i] += suffixsum[i + 1] + nums[i + 1];
+        }
+        return suffixsum;
+    }
+    vector<int> leftRightDifference(vector<int>& nums) {
+        vector<int> leftsum;
+        vector<int> rightsum;        
+        int n = nums.size();
+        
+        leftsum = findprefix(nums);
+        rightsum = findsuffix(nums);
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++){
+            // cout<<leftsum[i]<<" ";
+            cout<<rightsum[i]<<" ";
+            int diff = abs(leftsum[i] - rightsum[i]);
+            ans.push_back(diff);
+        }
+        return ans;
+    }
+};
