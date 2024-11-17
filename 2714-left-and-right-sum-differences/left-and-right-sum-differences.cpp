95df4f50eac5807c35af0e3ed239d1f737730821
Shubham Sharma <shubhamgetmail@gmail.com>
2024-11-17 23:53:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (61.36%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    vector<int> findprefix(vector<int>& nums){
-        int n = nums.size();
-        vector<int> prefixsum(n, 0);
-        for(int i = 1; i < nums.size(); i++){
-            prefixsum[i] += prefixsum[i - 1] + nums[i - 1]; 
-        }
-        return prefixsum;
-    }
-
-    vector<int> findsuffix(vector<int>& nums){
-        int n = nums.size();
-        vector<int> suffixsum(n, 0);
-        for(int i = n - 2; i >= 0; i--){
-            suffixsum[i] += suffixsum[i + 1] + nums[i + 1];
-        }
-        return suffixsum;
-    }
     vector<int> leftRightDifference(vector<int>& nums) {
-        vector<int> leftsum;
-        vector<int> rightsum;        
-        int n = nums.size();
-        
-        // leftsum = findprefix(nums);
-        // rightsum = findsuffix(nums);
-        
+        // calculating  total_sum
         int total_sum = 0;
         for(auto num : nums){
             total_sum += num;
         }
-        
+
+        // take 3 int --> prefixSum, suffixSum, total_sum
+        // suffixSum = total_Sum - prefixSum - nums[i]
+        int prefixSum = 0;
+        int n = nums.size();
         vector<int> ans;
-        int prefix_sum = 0;
+        // loop for finding ans vector
         for(int i = 0; i < n; i++){
-            int suffix_sum = total_sum - prefix_sum - nums[i];
-            int diff = abs(prefix_sum - suffix_sum); 
+            int suffixSum = total_sum - prefixSum - nums[i];
+            int diff = abs(prefixSum - suffixSum);
             // int diff = abs(leftsum[i] - rightsum[i]);
             ans.push_back(diff);
-            prefix_sum += nums[i];
+            prefixSum += nums[i];
         }
         return ans;
     }
