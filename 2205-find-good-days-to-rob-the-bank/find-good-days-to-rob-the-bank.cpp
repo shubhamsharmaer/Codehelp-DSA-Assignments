e52af907310cbd4470b5c8a10e96be2d3a78d54c
Shubham Sharma <shubhamgetmail@gmail.com>
2024-11-21 23:12:30 +0530

    Time: 6 ms (77.16%) | Memory: 92.7 MB (45.29%) - LeetSync

--- 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.cpp
+++ 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
         int n = security.size();
-        // prefix arr from i - time to i - 1
-        vector<int>prefix(n, 0);
-        prefix[0] = 0;
-        vector<int>suffix(n, 0);
-        suffix[n-1] = 0;
 
-        // finding the increasing arr
+        // take two sums vec
+        vector<int> prefixSum(n, 0);
+        // prefixSum[0] = 0;
+        vector<int> suffixSum(n, 0);
+        suffixSum[n-1] = 0;
+
+        // loop for non-increasing
         for(int i = 1; i < n; i++){
-            if(security[i] <= security[i-1]){
-                prefix[i] = prefix[i - 1] + 1;
+            if(security[i] <= security[i - 1]){
+                prefixSum[i] = prefixSum[i - 1] + 1;
             }
         }
 
-        // finding decreasing arr
+        // loop for non-decreasing
         for(int i = n - 2; i >= 0; i--){
-            if(security[i] <= security[i+1]){
-                suffix[i] = suffix[i + 1] + 1;
+            if(security[i] <= security[i + 1]){
+                suffixSum[i] = suffixSum[i + 1] + 1;
             }
         }
 
+        // check for penalty
         vector<int> ans;
         for(int i = 0; i < n; i++){
-            if(prefix[i] >= time && suffix[i] >= time){
+            if(prefixSum[i] >= time && suffixSum[i] >= time){
                 ans.push_back(i);
             }
         }
