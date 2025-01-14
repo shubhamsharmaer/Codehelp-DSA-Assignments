31f27a64bc0a98b93bb71ecf5c5888ef72c05b76
Shubham Sharma <shubhamgetmail@gmail.com>
2025-01-15 00:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (68.93%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,35 +11,49 @@
  */
 class Solution {
 public:
+    // taking global var
+    bool isbal = true; // assuming every tree is balanced
     int height(TreeNode* root) {
         // base case
         if(!root) return 0;
 
         int lh = height(root->left);
         int rh = height(root->right);
+
+        // check for false cond
+        if(isbal && abs(lh - rh) > 1) {
+            isbal = false;
+        }
+
         int ans = max(lh, rh) + 1;
         return ans;
     }
     bool isBalanced(TreeNode* root) {
+       height(root);
+       return isbal;
+       
+       
+       
+       
         // take base case
-        if(!root) return true;
+        // if(!root) return true;
         
         // for one case
-        int lh = height(root->left);
-        int rh = height(root->right);
+        // int lh = height(root->left);
+        // int rh = height(root->right);
         // if(abs(lh - rh) <= 1) return true;
-        int diff = abs(lh - rh);
-        bool currNode = diff <= 1;
+        // int diff = abs(lh - rh);
+        // bool currNode = diff <= 1;
 
         // baki recurssion sambhalega
-        bool leftH = isBalanced(root->left);
-        bool rightH = isBalanced(root->right);
+        // bool leftH = isBalanced(root->left);
+        // bool rightH = isBalanced(root->right);
         
         // we have 3 things --> currNode res, leftH, rightH
         // if anyone got true then final ans --> true
         // use OR gate
-        if(currNode && (leftH && rightH)) return true;
-        else return false;
+        // if(currNode && (leftH && rightH)) return true;
+        // else return false;
         
 
     }
