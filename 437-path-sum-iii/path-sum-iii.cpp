bac7e719156fae57f0c48e00431e680f59f95665
Shubham Sharma <shubhamgetmail+dev@gmail.com>
2025-02-20 23:55:43 +0530

    Time: 5 ms (76.52%) | Memory: 18.8 MB (81.79%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -12,21 +12,18 @@
 class Solution {
 public:
     int ans = 0;
-    void pathfromRoot(TreeNode* root, long long sum) {
+    void pathfromRoot(TreeNode* root, long long sum){
         if(!root) return;
 
-        if(sum == root->val) {
-            ans++;
-        }
+        if(sum == root->val) ans++;
         pathfromRoot(root->left, sum - root->val);
-        pathfromRoot(root->right, sum - root->val);
+        pathfromRoot(root->right, sum - root->val);        
     }
     int pathSum(TreeNode* root, long long targetSum) {
-        if(root) {
+        if(!root) return 0;
         pathfromRoot(root, targetSum);
         pathSum(root->left, targetSum);
         pathSum(root->right, targetSum);
-        }
         return ans;
     }
 };
